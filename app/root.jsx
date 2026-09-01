d0dc591fd2cc1847e744c22c3cae1a2c914f13a2
yashsolanki12 <yash@ecodesoft.com>
2026-09-01 14:53:51 +0530

    chore: wip 1 automated check issue

--- app/root.jsx
+++ app/root.jsx
@@ -1,28 +1,21 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
-import { StyledEngineProvider } from "@mui/material/styles";
-
-export const meta = () => {
-  return [{ name: "viewport", content: "width=device-width,initial-scale=1" }];
-};
 
 export default function App() {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta
-          name="emotion-insertion-point"
-          content=""
-          suppressHydrationWarning
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
         <Meta />
         <Links />
       </head>
-      <body suppressHydrationWarning>
-        <StyledEngineProvider injectFirst>
-          <Outlet />
-        </StyledEngineProvider>
+      <body>
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
