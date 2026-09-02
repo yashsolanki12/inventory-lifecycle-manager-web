b986b11f3fea67f309199ae83d0eef128c042119
yashsolanki12 <yash@ecodesoft.com>
2026-09-02 11:22:19 +0530

    chore: wip 4 automated check issue

--- app/root.jsx
+++ app/root.jsx
@@ -1,4 +1,37 @@
+// import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+// import { StyledEngineProvider } from "@mui/material/styles";
+
+// export const meta = () => {
+//   return [{ name: "viewport", content: "width=device-width,initial-scale=1" }];
+// };
+
+// export default function App() {
+//   return (
+//     <html lang="en" suppressHydrationWarning>
+//       <head>
+//         <meta charSet="utf-8" />
+//         <meta
+//           name="emotion-insertion-point"
+//           content=""
+//           suppressHydrationWarning
+//         />
+//         <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+//         <Meta />
+//         <Links />
+//       </head>
+//       <body suppressHydrationWarning>
+//         <StyledEngineProvider injectFirst>
+//           <Outlet />
+//         </StyledEngineProvider>
+//         <ScrollRestoration />
+//         <Scripts />
+//       </body>
+//     </html>
+//   );
+// }
+
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 export default function App() {
   return (
@@ -6,6 +39,11 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta
+          name="emotion-insertion-point"
+          content=""
+          suppressHydrationWarning
+        />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
         <link
           rel="stylesheet"
@@ -14,8 +52,10 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body suppressHydrationWarning>
+        <StyledEngineProvider injectFirst>
+          <Outlet />
+        </StyledEngineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
